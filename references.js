ddf5f7a6c17924bb5cf004b7c9599a35bbb68388
ShyBo <beau.shirdavani@bellevuecollege.edu>
2019-01-25 23:17:46 +0000

    find and return post data for a user, by email

--- references.js
+++ references.js
@@ -40,26 +40,37 @@ var User = mongoose.model("User", userSchema);
 
 
 // connect the post and user
-Post.create({
-    title: "other book",
-    content: "2972349582749805724"
-}, function(error, post){
-    // find a user to associate this with
-    User.findOne({email: "bob@gmail.com"}, function(err, foundUser){
-        if(err){
-            console.log(err);
-        }else{
-            // push this post into that users post array
-            foundUser.posts.push(post);
-            // save the user
-            foundUser.save(function(error, data){
-                if(error){
-                    console.log(error);
-                }else{
-                    console.log("data connected: ", data);
-                }
-            });
-        }
-    });
+// Post.create({
+//     title: "other book",
+//     content: "2972349582749805724"
+// }, function(error, post){
+//     // find a user to associate this with
+//     User.findOne({email: "bob@gmail.com"}, function(err, foundUser){
+//         if(err){
+//             console.log(err);
+//         }else{
+//             // push this post into that users post array
+//             foundUser.posts.push(post);
+//             // save the user
+//             foundUser.save(function(error, data){
+//                 if(error){
+//                     console.log(error);
+//                 }else{
+//                     console.log("data connected: ", data);
+//                 }
+//             });
+//         }
+//     });
+// });
+
+
+// find user
+// and find all posts for that user
+User.findOne({email: "bob@gmail.com"}).populate("posts").exec(function(error, user){
+    if(error){
+        console.log(error);
+    }else{
+        console.log(user); // this now shows full posts attributes, not just an ID
+    }
 });
 
