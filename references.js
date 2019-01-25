ebc7517976d1cbef72334ff82f7d9d30c3771986
ShyBo <beau.shirdavani@bellevuecollege.edu>
2019-01-25 23:13:13 +0000

    made new post, associated with user

--- references.js
+++ references.js
@@ -26,4 +26,40 @@ var userSchema = new mongoose.Schema({
 });
 var User = mongoose.model("User", userSchema);
 
+// User.create({
+//   email: "bob@gmail.com",
+//   name: "Robert Belcher"
+// });
+
+// Post.create({
+//     title: "How to coook the best burger",
+//     content: "blah blah blah blah meh"
+// }, function(error, post){
+//     console.log("posting: ", post);
+// });
+
+
+// connect the post and user
+Post.create({
+    title: "other book",
+    content: "2972349582749805724"
+}, function(error, post){
+    // find a user to associate this with
+    User.findOne({email: "bob@gmail.com"}, function(err, foundUser){
+        if(err){
+            console.log(err);
+        }else{
+            // push this post into that users post array
+            foundUser.posts.push(post);
+            // save the user
+            foundUser.save(function(error, data){
+                if(error){
+                    console.log(error);
+                }else{
+                    console.log("data connected: ", data);
+                }
+            });
+        }
+    });
+});
 
